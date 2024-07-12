1457fc246cf14a0149076b7ad98c439959c8c618
Terence Svubure <tsvubure@gmail.com>
2024-07-12 18:55:14 +0200

    added projects section (#3)

---
+++ src/components/projects.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        id: 1,
+        imageUrl: "blackjack.jpg",
+        title: "Black Jack",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque sint, assumenda sequi",
+        tech: "Lorem, ipsum dolor sit amet consectetur",
+        link: "https://github.com/tsvubz/blackjack_game",
+    },
+    {
+        id: 2,
+        imageUrl: "blackjack.jpg",
+        title: "Black Jack",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque sint, assumenda sequi",
+        tech: "Lorem, ipsum dolor sit amet consectetur",
+        link: "https://github.com/tsvubz/blackjack_game",
+    },
+    {
+        id: 3,
+        imageUrl: "blackjack.jpg",
+        title: "Black Jack",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque sint, assumenda sequi",
+        tech: "Lorem, ipsum dolor sit amet consectetur",
+        link: "https://github.com/tsvubz/blackjack_game",
+    },
+    {
+        id: 4,
+        imageUrl: "blackjack.jpg",
+        title: "Black Jack",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque sint, assumenda sequi",
+        tech: "Lorem, ipsum dolor sit amet consectetur",
+        link: "https://github.com/tsvubz/blackjack_game",
+    },
+    {
+        id: 5,
+        imageUrl: "blackjack.jpg",
+        title: "Black Jack",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque sint, assumenda sequi",
+        tech: "Lorem, ipsum dolor sit amet consectetur",
+        link: "https://github.com/tsvubz/blackjack_game",
+    }   
+]
