ea993dfe72b13b0235644218131ce793e6ce5216
Terence Svubure <tsvubure@gmail.com>
2024-08-15 16:00:24 +0200

    added Tech skills (#12)

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import Hero from "./components/molecules/Hero/Hero"
 import Footer from "./components/organisms/Footer/Footer"
 import Navbar from "./components/organisms/Nav/Navbar"
 import Projects from "./components/organisms/Projects/Projects"
-
-
-
+import Tech from "./components/organisms/Tech/Tech"
 
 
 function App() {
@@ -18,6 +16,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Tech />
       <Projects />
       <Contact />
       <Footer />
---
+++ src/components/skills.js
@@ -0,0 +1,75 @@
+export default [
+    {
+        id: 1,
+        icon: "html.svg",
+        value: '90',
+        max: '100',
+    },
+    {
+        id: 2,
+        icon: "css.svg",
+        value: '90',
+        max: '100',
+    },
+    {
+        id: 3,
+        icon: "javascript.svg",
+        value: '90',
+        max: '100',
+    },
+    {
+        id: 4,
+        icon: "react-native.svg",
+        value: '75',
+        max: '100',
+    },
+    {
+        id: 5,
+        icon: "python.svg",
+        value: '65',
+        max: '100',
+    },
+    {
+        id: 6,
+        icon: "nodejs.svg",
+        value: 60,
+        max: 100,
+    },
+    {
+        id: 7,
+        icon: "terraform.svg",
+        value: 70,
+        max: 100,
+    },
+    {
+        id: 8,
+        icon: "docker.svg",
+        value: 70,
+        max: 100,
+    },
+    {
+        id: 9,
+        icon: "github.svg",
+        value: 90,
+        max: 100,
+    },
+    {
+        id: 10,
+        icon: "postgresql.svg",
+        value: 55,
+        max: 100,
+    },
+    {
+        id: 11,
+        icon: "microsoft-sql-server.svg",
+        value: 75,
+        max: 100,
+    },
+    {
+        id: 12,
+        icon: "csharp.svg",
+        value: 55,
+        max: 100,
+    },
+
+]
